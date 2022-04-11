3ef557af9b07e9b5a3f4c93938dac894693c993c
Akex Sorkin <Alesorkin1980@gmail.com>
2022-04-11 14:42:02 +0300

    Books create, index, new

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 
 app.set('view engine', 'ejs')
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
 
 
 const mongoose = require('mongoose')
+
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
 const db = mongoose.connection
 db.on('error',error=>console.error(error))
@@ -27,4 +29,5 @@ db.once('open',()=>console.log('Connected'))
 
 app.use('/',indexRouter)
 app.use('/authors',authorRouter)
+app.use('/books',bookRouter)
 app.listen(process.env.PORT || 3000)
